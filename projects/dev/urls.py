515940689755c090b1a4ba90ad19ef8a51019d04
Martyn CLEMENT <Martyn CLEMENT@localhost>
2012-04-19 18:55:03 +0200

    formulas

--- projects/dev/urls.py
+++ projects/dev/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic import TemplateView
 from brew.views import RecipeListView
 
 admin.autodiscover()
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
     (r'^units/', include('units.urls')),
     (r'^accounts/', include('invitation.urls')),
     (r'^accounts/', include('registration_backend.urls')),
+    url(r'^how-it-works/', TemplateView.as_view(template_name="how.html"), name="how_it_works"),
 
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^admin/', include(admin.site.urls)),
