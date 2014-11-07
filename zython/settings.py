cf957fa6daad9c686002ef46a81b97845538b29f
Martyn CLEMENT <Martyn CLEMENT@localhost>
2014-11-07 11:45:01 +0100

    Useless decimal separator

--- zython/settings.py
+++ zython/settings.py
@@ -131,7 +131,6 @@ LANGUAGES = (
 USE_I18N = True
 USE_L10N = True
 USE_TZ = False
-USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
