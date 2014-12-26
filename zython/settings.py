8b67b8836add642157ea5c3fefb4066d95dc42be
Martyn CLEMENT <Martyn CLEMENT@localhost>
2014-12-26 15:14:56 +0100

    Custom LocalMiddleware bypassed

--- zython/settings.py
+++ zython/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "account.middleware.LocaleMiddleware",
     "account.middleware.TimezoneMiddleware",
 )
 
